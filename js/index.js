a4e43c428849692aec9f67e575b7fc145f65d429
yAnalytics <dennis@yanalytics.eu>
2013-10-31 20:59:16 +0100

    Added CLIENT_ID and CLIENT_SECRET

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var CLIENT_ID = "";
-var CLIENT_SECRET = "";
+var CLIENT_ID = "122670225392-t8qh5ennnrqim2j7vrbc3vfa1td3sq5d.apps.googleusercontent.com";
+var CLIENT_SECRET = "EE3T2nrELtAGGJIl9stXQq-2";
 var CLIENT_SCOPES = [
     'https://www.googleapis.com/auth/yt-analytics.readonly',
     'https://www.googleapis.com/auth/youtube.readonly'
