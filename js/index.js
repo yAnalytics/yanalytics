17fdc33ed8a3dbaeb379973f0f3081a9da5cf936
yAnalytics <dennis@yanalytics.eu>
2013-10-31 21:28:31 +0100

    Teest

--- js/index.js
+++ js/index.js
@@ -6,48 +6,8 @@ var CLIENT_SCOPES = [
 ];
 
 var user  = {
-	
 	authorize : function() {
-		var deferred = $.Deferred();
-		
-		var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
-                       client_id: CLIENT_ID,
-                       redirect_uri: "http://localhost",
-                       response_type: 'code',
-                       scope: CLIENT_SCOPES });
-       
-       var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
-       $(authWindow).on('loadstart', function(e){
-       	
-       	  var url = e.originalEvent.url;
-          var code = /\?code=(.+)$/.exec(url);
-          var error = /\?error=(.+)$/.exec(url);
-
-          if (code || error) {
-             authWindow.close();
-          }
-          
-          if (code) {
-          	$.post('https://accounts.google.com/o/oauth2/token', {
-          	        code: code[1],
-          	        client_id: CLIENT_ID,
-          	        client_secret: CLIENT_SECRET,
-          	        redirect_uri: "http,://localhost",
-          	        grant_type: 'authorization_code' 
-          	
-          	}).done(function(data) {
-          		deferred.resolve(data);
-          	}).fail(function(response) {
-          		deferred.reject(response.responseJSON);
-          	});
-          	
-          } else if(error) {
-          	deferred.reject({error: error[1]});
-          }
-       	
-       });
-       
-       return deferred.promise();
+		window.open('http://www.google.de', '_blank', 'toolbar=no,location=no');
 	}
 };
 
@@ -58,6 +18,5 @@ $(document).on('deviceready', function() {
 	
 	$(loginButton).on('click', function() 
 	user.authorize();
-	
 	});
 });
