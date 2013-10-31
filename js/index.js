e53d91a97548f18ae9c4fcb868c76571f297d849
yAnalytics <dennis@yanalytics.eu>
2013-10-31 22:04:07 +0100

    Geiler :D

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 var CLIENT_ID = "122670225392-t8qh5ennnrqim2j7vrbc3vfa1td3sq5d.apps.googleusercontent.com";
 var CLIENT_SECRET = "EE3T2nrELtAGGJIl9stXQq-2";
 var CLIENT_SCOPES = [
-    'https://www.googleapis.com/auth/yt-analytics.readonly',
-    'https://www.googleapis.com/auth/youtube.readonly'
+    'https://www.googleapis.com/auth/yt-analytics.readonly'
 ];
 var CLIENT_REDIRECT_URI = "http://localhost";
 
