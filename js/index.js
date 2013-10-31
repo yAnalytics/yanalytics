b8da742c3937cc9b6a741a5d5dc1830d00d6c50a
yAnalytics <dennis@yanalytics.eu>
2013-10-31 20:48:34 +0100

    Added user class

--- js/index.js
+++ js/index.js
@@ -1,76 +1,63 @@
-var googleapi = {
-    authorize: function(options) {
-        var deferred = $.Deferred();
+var CLIENT_ID = "";
+var CLIENT_SECRET = "";
+var CLIENT_SCOPES = [
+    'https://www.googleapis.com/auth/yt-analytics.readonly',
+    'https://www.googleapis.com/auth/youtube.readonly'
+];
 
-        //Build the OAuth consent page URL
-        var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
-            client_id: options.client_id,
-            redirect_uri: options.redirect_uri,
-            response_type: 'code',
-            scope: options.scope
-        });
+var user  = {
+	
+	authorize : function() {
+		var deferred = $.deferred();
+		
+		var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
+                       client_id: CLIENT_ID,
+                       redirect_uri: "http://localhost",
+                       response_type: 'code',
+                       scope: CLIENT_SCOPES });
+       
+       var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
+       (authWindow).on('loadstart', function(e){
+       	
+       	  var url = e.originalEvent.url;
+          var code = /\?code=(.+)$/.exec(url);
+          var error = /\?error=(.+)$/.exec(url);
 
-        //Open the OAuth consent page in the InAppBrowser
-        var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
-
-        //The recommendation is to use the redirect_uri "urn:ietf:wg:oauth:2.0:oob"
-        //which sets the authorization code in the browser's title. However, we can't
-        //access the title of the InAppBrowser.
-        //
-        //Instead, we pass a bogus redirect_uri of "http://localhost", which means the
-        //authorization code will get set in the url. We can access the url in the
-        //loadstart and loadstop events. So if we bind the loadstart event, we can
-        //find the authorization code and close the InAppBrowser after the user
-        //has granted us access to their data.
-        $(authWindow).on('loadstart', function(e) {
-            var url = e.originalEvent.url;
-            var code = /\?code=(.+)$/.exec(url);
-            var error = /\?error=(.+)$/.exec(url);
-
-            if (code || error) {
-                //Always close the browser when match is found
-                authWindow.close();
-            }
-
-            if (code) {
-                //Exchange the authorization code for an access token
-                $.post('https://accounts.google.com/o/oauth2/token', {
-                    code: code[1],
-                    client_id: options.client_id,
-                    client_secret: options.client_secret,
-                    redirect_uri: options.redirect_uri,
-                    grant_type: 'authorization_code'
-                }).done(function(data) {
-                    deferred.resolve(data);
-                }).fail(function(response) {
-                    deferred.reject(response.responseJSON);
-                });
-            } else if (error) {
-                //The user denied access to the app
-                deferred.reject({
-                    error: error[1]
-                });
-            }
-        });
-
-        return deferred.promise();
-    }
+          if (code || error) {
+             authWindow.close();
+          }
+          
+          if (code) {
+          	$.post('https://accounts.google.com/o/oauth2/token', {
+          	        code: code[1],
+          	        client_id: CLIENT_ID,
+          	        client_secret: CLIENT_SECRET,
+          	        redirect_uri: "http,://localhost",
+          	        grant_type: 'authorization_code' 
+          	
+          	}).done(function(data) {
+          		deferred.resolve(data);
+          	}).fail(function(response) {
+          		deferred.reject(response.responseJSON);
+          	});
+          	
+          } else if(error) {
+          	deferred.reject({error: error[1]});
+          }
+       	
+       });
+       
+       return deferred.promise();
+	}
 };
 
 $(document).on('deviceready', function() {
-    var $loginButton = $('#login a');
-    var $loginStatus = $('#login p');
-
-    $loginButton.on('click', function() {
-        googleapi.authorize({
-            client_id: '122670225392-t8qh5ennnrqim2j7vrbc3vfa1td3sq5d.apps.googleusercontent.com',
-            client_secret: 'EE3T2nrELtAGGJIl9stXQq-2',
-            redirect_uri: 'http://localhost',
-            scope: 'https://www.googleapis.com/auth/analytics.readonly'
-        }).done(function(data) {
-            $loginStatus.html('Access Token: ' + data.access_token);
-        }).fail(function(data) {
-            $loginStatus.html(data.error);
-        });
-    });
+	var $loginButton = "#login a";
+	var $loginStatus = "#login p";
+	
+	user.authorize().done(function(data){
+		$loginStatus.html = 'Access Token: ' + data.access_token;
+	}).fail(function(data) {
+		$loginStatus.html(data.error);
+	});
 });
