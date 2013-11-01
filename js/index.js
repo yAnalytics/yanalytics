1168de16acb8fee0c784f3d9f4e1cef8a693f545
yAnalytics <dennis@yanalytics.eu>
2013-11-01 01:07:55 +0100

    mmm

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var CLIENT_ID = "122670225392-t8qh5ennnrqim2j7vrbc3vfa1td3sq5d.apps.googleusercontent.com";
 var CLIENT_SECRET = "EE3T2nrELtAGGJIl9stXQq-2";
-var CLIENT_SCOPES = 'https://www.googleapis.com/auth/yt-analytics.readonly';
+var CLIENT_SCOPES = 'https://www.googleapis.com/auth/analytics.readonly';
 var CLIENT_REDIRECT_URI = "http://localhost";
 
 var token;
