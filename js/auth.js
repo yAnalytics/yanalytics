b7c5284a99c23e68e3d1e5ee35fe7c4b97962225
Drunken Monkey <drunkenmonkeydev@gmail.com>
2013-12-02 18:53:34 +0100

    scopes

--- js/auth.js
+++ js/auth.js
@@ -8,9 +8,7 @@
 var CLIENT_ID = "122670225392-t8qh5ennnrqim2j7vrbc3vfa1td3sq5d.apps.googleusercontent.com";
 var CLIENT_SECRET = "EE3T2nrELtAGGJIl9stXQq-2";
 var CLIENT_REDIRECT_URI = "http://localhost";
-var CLIENT_SCOPES =  ['https://www.googleapis.com/auth/userinfo.profile',
-                      'https://www.googleapis.com/auth/yt-analytics.readonly',
-                      'https://www.googleapis.com/auth/youtube.readonly'];
+var CLIENT_SCOPES =  'https://www.googleapis.com/auth/yt-analytics.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly';
 
 var $loginButton = $('#login a');
 var $loginStatus = $('#login p');
