4976394e7c10b81799fcd221867d0da07ccb0daa
Drunken Monkey <drunkenmonkeydev@gmail.com>
2013-12-02 19:24:05 +0100

    test :D

--- js/channel.js
+++ js/channel.js
@@ -1,9 +1,7 @@
 function getUser(options) {
-	$loginStatus.append('HEL');
 	return $.getJSON('https://www.googleapis.com/oauth2/v1/userinfo', options);
 }
 
 function getChannel(options) {
 	return $.getJSON('https://www.googleapis.com/youtube/v3/channels', options);
-	
 }
