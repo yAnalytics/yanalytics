6c8441c73fb4274e6e5282ea44b9da411d233db7
Drunken Monkey <drunkenmonkeydev@gmail.com>
2013-12-03 13:30:46 +0100

    Created testview

--- js/index.js
+++ js/index.js
@@ -1,21 +1,37 @@
+var app = {
+	init : function() {
+		 var $loginButton = $('#login a');
+		 var $loginStatus = $('#login p');
+		 var $logoutButton = $('#logout a');
+		 
+		 $logoutButton.hide();
+		 
+		 $loginButton.on('click', function() {
+		 auth.authorize();
+		 });
+		 
+		 auth.checkToken().done(function() {
+		 	$loginButton.hide();
+		 	$loginStatus.append('Du bist erfolgreich eingeloggt.');
+		 	app.showView();
+		 	}).fail(function() {
+		 		auth.authorize();
+		 		});
+	},
+	
+	showView: function() {
+		getUser({
+			access_token: localStorage.access_token
+		}).done(function(data) {
+			$loginStatus.append('Hallo ' + data.name);
+		}).fail(function() {
+			app.init();
+		});
+	}
+	
+};
+
+
 $(document).on('deviceready', function() {
-    var $loginButton = $('#login a');
-    var $loginStatus = $('#login p');
-	var $logoutButton = $('#logout a');
-	
-	$logoutButton.hide();
-	
-	$loginButton.on('click', function() {
-		user.login();
-	});
-	
-	user.checkToken().done(function() {
-		$loginButton.hide();
-		$loginStatus.append('Du bist erfolgreich eingeloggt.');
-	}).fail(function() {
-		$loginStatus.append('Nicht richtig.');
-		user.login();
-	});
-	
-	
+	app.init();	
 });
