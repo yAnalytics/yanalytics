6ae9fd042d9e8440f6d82c3ca93774e1d19e2bf1
Drunken Monkey <drunkenmonkeydev@gmail.com>
2013-12-04 19:09:52 +0100

    asdf

--- js/index.js
+++ js/index.js
@@ -18,10 +18,10 @@ var app = {
 		 		access_token: data.access_token
 		 	}).done(function(data) {
 		 		$loginStatus.append('Hallo ' + data.name);
-		 		status = true;
 		 	}).fail(function(data) {
 		 		$loginStatus.append('Error: ' + data.error);
 		 	});
+		 	status = true;
 		 }).fail(function(data) {
 		 	$loginStatus.append('xD');
 		 });
