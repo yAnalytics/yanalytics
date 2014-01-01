c4c11471df8f10bde41284e47b43edff2e21474b
Dennis N <dennis@yanalytics.eu>
2014-01-01 13:32:14 +0100

    sdf

--- www/js/googleapi/channel.js
+++ www/js/googleapi/channel.js
@@ -50,6 +50,7 @@ viewsLastMonth: function() {
 	   	$loginStatus.append("Views: " + data.rows[0]);
 	   	deferred.resolve(data.rows);
 	   }).fail(function(error) {
+	   	$loginStatus.append("Fehler" + error.errors.message);
 	   	deferred.resolve(error);
 	   });
     } else {
