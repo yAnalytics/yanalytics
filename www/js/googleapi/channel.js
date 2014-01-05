d5b1b1d135a66de91b191564cad2d1374cec514e
Dennis N <dennis@yanalytics.eu>
2014-01-05 12:16:25 +0100

    Deleted useless code

--- www/js/googleapi/channel.js
+++ www/js/googleapi/channel.js
@@ -3,78 +3,7 @@
  * © (2013) Dennis Nemec & Raphael Hauger
  *
  */
-jQuery.ajaxSetup({
-   async: false
-});
 
-var channelReqLink = "https://www.googleapis.com/youtube/v3/channels";
-var channelPlaylistItems = "https://www.googleapis.com/youtube/v3/playlistItems";
-var access_token = "ya29.1.AADtN_VrBhyJ4kXvo_bF6Urk8x6WK88jegc23FT79tIxuXOsI2CbH7m0AFB99w";
-
-
-var channel = {	
-id : function() {
-	var deferred = $.Deferred();
-		
-	$.getJSON('https://www.googleapis.com/youtube/v3/channels', 
-	{
-		access_token : localStorage.access_token,
-		mine: true,
-		part: 'id,snippet'
-	}
-	).done(function(data) {
-		tmp = data.items[0].id;
-		deferred.resolve(data.items[0].id);
-	}).fail(function(data) {
-		deffered.reject();
-	});
-	
-	return deferred.promise();
-},
-
-name: function(options) {
-},
-
-logo : function(options) {
-	
-},
-
-viewsLastMonth: function() {
-    var deferred = $.Deferred();
-    
-    if (localStorage.channelId) {
-    	
-	   $.getJSON('https://www.googleapis.com/youtube/analytics/v1/reports', 
-	   {
-	   	ids: 'channel==' + localStorage.channelId, 
-	   	access_token: localStorage.access_token,
-	    'start-date': '2013-10-01',
-	   	'end-date': '2013-12-31',
-	   	metrics: 'views'
-	   }
-	   
-	   ).done(function(data) {
-	   	$loginStatus.append("<br><b>Views: " + data.rows[0] + "</b>");
-	   	deferred.resolve(data.rows);
-	   }).fail(function(response) {
-	   	$loginStatus.append("Something went wrong:");
-	   	deferred.resolve(response);
-	   });
-    } else {
-    	deferred.reject();
-    }
-    
-    return deferred.promise();
-},
-
-getVideoIdByName: function() {
-	
-}
-};
-
-
-
-// new kind of fetching data
 function channelData() {
 	// properties
 	var statistics = channelReq.statistics();
