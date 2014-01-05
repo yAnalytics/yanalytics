92820424fee7072bf72bb8f2f36bc6ae8967d47d
Dennis N <dennis@yanalytics.eu>
2014-01-05 14:51:08 +0100

    lpoi9

--- www/js/index.js
+++ www/js/index.js
@@ -66,7 +66,7 @@ var app = {
         	$loginStatus.append("ID: " + info.id);
         	$loginStatus.append("<br>Upload-ID: " + info.uploads); 	
         	$loginStatus.append("<br>Titel: " + info.title);
-        	$loginStatus.append("<br>Menge an Videos: " + info.thumbnailUrlDefault);
+        	$loginStatus.append("<br>Menge an Videos: " + info.uploads);
         	$loginStatus.append("<br>Beschreibung: " + info.subscribers);
         	$loginStatus.append("<br><img src='"+ info.bannerUrlMobileHd + "' alt='thumbnail' width='100%' height='50px'>");
 	}
