d9b58a627a1ae8c507745f38d919b70d052383ce
Dennis N <dennis@yanalytics.eu>
2014-01-05 12:11:31 +0100

    Added new scope for auditDetails (https://www.googleapis.com/auth/youtubepartner-channel-audit)

--- www/js/googleapi/info.js
+++ www/js/googleapi/info.js
@@ -15,5 +15,5 @@
 var CLIENT_ID = "122670225392-t8qh5ennnrqim2j7vrbc3vfa1td3sq5d.apps.googleusercontent.com";
 var CLIENT_SECRET = "EE3T2nrELtAGGJIl9stXQq-2";
 var CLIENT_REDIRECT_URI = "http://localhost";
-var CLIENT_SCOPES =  'https://www.googleapis.com/auth/yt-analytics.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/yt-analytics-monetary.readonly';
+var CLIENT_SCOPES =  'https://www.googleapis.com/auth/yt-analytics.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/yt-analytics-monetary.readonly https://www.googleapis.com/auth/youtubepartner-channel-audit';
 var API_KEY = 'AIzaSyDpN1tfxXXS_GsRw50jaj-2TIKLKYHjnTk';
